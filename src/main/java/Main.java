e2b710339137e719d0ab53e9daebf98f70fe13a9
EjjeBoi <ejje.ejje@list.ru>
2023-04-17 22:37:36 +0300

    New version of project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,16 +5,15 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
         // Создаем список объектов Person, считанных из файла "data"
         String separator = File.separator;
-        List<Person> persons = PersonReader.read(separator + "Users" + separator + "RedmiBook" + separator + "IdeaProjects" + separator + "PersonalDataTest" + separator + "src" + separator + "data");
+        List<Person> persons = PersonReader.read("data");
         // Фильтруем список, оставляя только объекты с waterCount > 100
-        List<Person> filteredPersons = PersonProcessor.filterByElectroCount1(persons, 300);
+        List<Person> filteredPersons = PersonalDataFilter.filterByElectroCount1(persons, 300);
         // Выводим отфильтрованный список на экран
         filteredPersons.forEach(System.out::println);
     }
@@ -23,7 +22,7 @@ public class Main {
     public static class PersonReader {
 
         public static List<Person> read(String data) throws FileNotFoundException {
-            Scanner scanner = new Scanner(new File("C:\\Users\\RedmiBook\\IdeaProjects\\PersonalDataTest\\src\\data"));
+            Scanner scanner = new Scanner(new File("data"));
             List<Person> persons = new ArrayList<>();
             scanner.nextLine();
             while (scanner.hasNextLine()) {
@@ -44,38 +43,6 @@ public class Main {
         }
     }
 
-    public static class PersonProcessor {
-
-        public static List<Person> filterByWaterCount(List<Person> persons, int waterCount) {
-            return persons.stream()
-                    .filter(person -> person.getWaterCount() > waterCount)
-                    .collect(Collectors.toList());
-        }
-
-        public static List<Person> filterByGasCount1(List<Person> persons, int gasCount1) {
-            return persons.stream()
-                    .filter(person -> person.getGasCount1() > gasCount1)
-                    .collect(Collectors.toList());
-        }
-
-        public static List<Person> filterByGasCount2(List<Person> persons, int gasCount2) {
-            return persons.stream()
-                    .filter(person -> person.getGasCount2() > gasCount2)
-                    .collect(Collectors.toList());
-        }
-
-        public static List<Person> filterByElectroCount1(List<Person> persons, int electroCount1) {
-            return persons.stream()
-                    .filter(person -> person.getElectroCount1() > electroCount1)
-                    .collect(Collectors.toList());
-        }
-
-        public static List<Person> filterByElectroCount2(List<Person> persons, int electroCount2) {
-            return persons.stream()
-                    .filter(person -> person.getElectroCount2() > electroCount2)
-                    .collect(Collectors.toList());
-        }
-    }
     @Data
     @AllArgsConstructor
     public static class Person {
